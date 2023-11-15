35aef13a188499a3c59f1cdb2092feb75cd917e7
LauraUC3M <1postlaura@gmail.com>
2023-11-15 13:35:36 +0900

    Show announcements on dashboard

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -7,22 +7,60 @@ const mongoose = require('mongoose');
  * Dashboard
  */
 exports.dashboard = async(req, res) => {
+
+  // 5 announcements per page
+  let perPage = 5;
+  let page = req.query.page || 1;
+  
   const locals = {
     title: 'Dashboard',
     description: 'Base App for Habitat Connect'
   }
 
   try {
-    const announcements = await Announcement.find({});
-    
-    res.render('dashboard/index', {
-      userName: req.user.firstName,
-      locals,
-      announcements,
-      layout: 'layouts/dashboard'
-    });
+
+    Announcement.aggregate([
+      {
+        $sort: {
+          createdAt: -1 // newest announcement first
+        }
+      },
+      {
+        // user can see their announcements
+        // prob change this
+        $match: {
+          user: mongoose.Types.ObjectId(req.user.id)
+        }
+      },
+      {
+        // max title 30 char
+        // max body 100 char
+        // this is just test,
+        // will need to change it
+        $project: {
+          title: { $substr: ['$title', 0, 30]},
+          body: { $substr: ['$body', 0, 100]}
+        }
+      }
+    ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec(function(err, announcements){
+      Announcement.count().exec(function (err, count){
+        if (err) return next(err);
+
+        res.render('dashboard/index', {
+          userName: req.user.firstName,
+          locals,
+          announcements,
+          layout: 'layouts/dashboard',
+          current: page,
+          pages: Math.ceil(count / perPage)
+        });
+      })
+    })
     
   } catch (error) {
-    
+    console.log(error);
   }
 }
