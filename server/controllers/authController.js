3ff4bbbbc695b8e408b0d4d9de2e6ce597b31ef1
pleskym <marcel.plesky68@gmail.com>
2023-11-22 17:11:42 +0900

    Added Birthday Validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -261,6 +261,30 @@ exports.authPostCompleteProfile = async (req, res) => {
     errors.push("Room Number must be between 300 and 500.");
   }
 
+  // birthdate validation
+  const isValidDate = (dateString) => {
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!regex.test(dateString)) {
+      return { isValid: false, errorMessage: "Invalid birthdate format. Please choose the date from the calendar." };
+    }
+
+    const selectedDate = new Date(dateString);
+    const currentDate = new Date();
+
+    // Compare with the current date
+    if (selectedDate > currentDate) {
+      return { isValid: false, errorMessage: "Birthdate cannot be in the future. Please choose a valid past date from the calendar." };
+    }
+
+    return { isValid: true, errorMessage: "" };
+  };
+
+  const dateValidationResult = isValidDate(birthdate);
+  if (!dateValidationResult.isValid) {
+    errors.push(dateValidationResult.errorMessage);
+  }
+
+
   if (errors.length > 0) {
     return res.render('auth/complete-profile', { 
         errors: errors
--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -6,24 +6,24 @@ const User = require('../models/User');
  * profile
  */
 exports.profile = async (req, res) => {
-    
-    const locals = {
-        title: 'Profile',
-        description: 'Profile page'
-    };
 
-    const user = await User.findById({ _id: req.user._id});
+  const locals = {
+    title: 'Profile',
+    description: 'Profile page'
+  };
 
-    try {
-        res.render('profile/index', {
-            locals,
-            user,
-            layout: 'layouts/profile',
-            errors: null,
-          }); 
-    } catch (error) {
-      console.log(error);
-    }
+  const user = await User.findById({ _id: req.user._id });
+
+  try {
+    res.render('profile/index', {
+      locals,
+      user,
+      layout: 'layouts/profile',
+      errors: null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 
@@ -31,7 +31,7 @@ exports.profile = async (req, res) => {
  * PUT /
  * update profile
  */
-exports.profileUpdate = async(req, res) => {  
+exports.profileUpdate = async (req, res) => {
 
   // retrieve user data
   const { username, roomNumber, birthdate } = req.body;
@@ -39,36 +39,61 @@ exports.profileUpdate = async(req, res) => {
 
   // username validation
   if (!/^[^\s]{1,12}$/.test(username)) {
-      errors.push("Username cannot be longer than 12 characters.");
+    errors.push("Username cannot be longer than 12 characters.");
   }
 
   // roomnumber validation
   if (!/^(3[0-9]{2}|4[0-9]{2}|500)$/.test(roomNumber)) {
-      errors.push("Room Number must be between 300 and 500.");
+    errors.push("Room Number must be between 300 and 500.");
   }
 
+  // birthdate validation
+  const isValidDate = (dateString) => {
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!regex.test(dateString)) {
+      return { isValid: false, errorMessage: "Invalid birthdate format. Please choose the date from the calendar." };
+    }
+
+    const selectedDate = new Date(dateString);
+    const currentDate = new Date();
+
+    // Compare with the current date
+    if (selectedDate > currentDate) {
+      return { isValid: false, errorMessage: "Birthdate cannot be in the future. Please choose a valid past date from the calendar." };
+    }
+
+    return { isValid: true, errorMessage: "" };
+  };
+
+  const dateValidationResult = isValidDate(birthdate);
+  if (!dateValidationResult.isValid) {
+    errors.push(dateValidationResult.errorMessage);
+  }
+
+
   if (errors.length > 0) {
-      const user = await User.findById(req.user.id);
-      return res.render('profile/index', {
-          locals: {
-              title: 'Profile',
-              description: 'Profile page'
-          },
-          user,
-          layout: 'layouts/profile',
-          errors,
-      });
+    const user = await User.findById(req.user.id);
+    return res.render('profile/index', {
+      locals: {
+        title: 'Profile',
+        description: 'Profile page'
+      },
+      user,
+      layout: 'layouts/profile',
+      errors,
+    });
   }
 
   try {
     await User.findByIdAndUpdate(
       { _id: req.user.id },
-      { username: req.body.username,
+      {
+        username: req.body.username,
         roomNumber: req.body.roomNumber,
         birthdate: req.body.birthdate
       });
 
-      res.redirect('/profile');
+    res.redirect('/profile');
   } catch (error) {
     console.log(error);
   }
